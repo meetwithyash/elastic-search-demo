595d97efb65a49b0828822f9d0c8fa858bbdf688
yash khokhaneshiya <yash.khokhaneshiya@artoon.in>
2022-04-16 15:21:46 +0530

    server.js changed

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-require("dotenv").config();
-
-const path = require("path");
 const express = require("express");
-
-const searchController = require("./controller/searchController");
-const prepareDataAndIndex = require("./utils/prepareDataAndIndex");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/*", (req, res, next) => {
+	res.send("Hello world!");
+});
 
-app.get("/search", searchController.runSearchFilter);
-
-prepareDataAndIndex().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server is running on PORT ${PORT}!`);
-	});
+app.listen(PORT, () => {
+	console.log(`Server is running on PORT ${PORT}!`);
 });
